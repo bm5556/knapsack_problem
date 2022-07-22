6a5e37fbee8983fae4d8dd88a437971cbf348f4f
bm5556 <85606721+bm5556@users.noreply.github.com>
2022-07-23 00:00:14 +0900

    Update 0-1_knapsack_problem.py

--- 0-1_knapsack_problem.py
+++ 0-1_knapsack_problem.py
@@ -1,18 +1,27 @@
 class KNAPSACK:
-    def __init__(self, n: int, max_weight: int):
-        self.max_weight = max_weight
-        self.items = list()
-        for _ in range(n):
+    def __init__(self, N, K) -> None:
+        self.N = N
+        self.MAX_WEIGHT = K
+        self.items = list() #self.items[i][0] == weight, self.items[i][1] == value
+        for _ in range(N):
             weight, value = map(int, input().split())
-            self.items.append(list(weight, value))
-        self.max_value = self.find_max_value(n)
-
-    def find_max_value(self, n: int, max_value = 0):
-        if n <= 0: return max_value
-        elif self.items[0]:
-            return self.find_max_value(n-1, max_value)
+            self.items.append([weight, value])
+        self.max_value = self.find_max_value(self.MAX_WEIGHT)
         
+
+    def find_max_value(self, extra_weight: int, i: int = 0, max_value: int = 0) -> int:
+    #find_max_value(i, max_value, extra_weight) means the maximum value 
+    #that can be obtain from previous i+1 items
+        if extra_weight < 0: return 0
+        if i == self.N: return max_value
+        if self.items[i][0] > extra_weight:
+            return self.find_max_value(extra_weight, i+1, max_value)
+        else: # self.items[i][0] <= self.MAX_WEIGHT
+            left = self.find_max_value(extra_weight, i+1, max_value)
+            right = self.find_max_value(extra_weight - self.items[i][0], i+1, max_value) + self.items[i][1]
+            return max(left, right)
 
 if __name__ == '__main__':
     N, K = map(int, input().split())
     KS = KNAPSACK(N, K)
+    print(KS.max_value)
